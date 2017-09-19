d900861db2eb07f2b0b67d4bc0c1a2d6382254dc
Sara Hopper <sara.hopper@gmail.com>
2017-09-19 20:05:50 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_attached_file :song_album_cover, default_url: ':style/default.png', styles: { thumb: "100x100>" }
+  has_attached_file :song_album_cover, default_url: ':style/default.gif', styles: { thumb: "100x100>" }
   validates_attachment_content_type :song_album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
